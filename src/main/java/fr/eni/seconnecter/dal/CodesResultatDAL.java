453b264cc43dc9fdfacb75b89226ba216e1d707c
Julien MOREAU <jmoreau@85.cerfrance.fr>
2022-08-01 16:41:09 +0200

    Initialisation du projet

---
+++ src/main/java/fr/eni/seconnecter/dal/CodesResultatDAL.java
@@ -0,0 +1,24 @@
+package fr.eni.seconnecter.dal;
+
+/**
+ * Les codes disponibles sont entre 10000 et 19999 
+ */
+
+public class CodesResultatDAL {
+
+	/**
+	 * Echec général quand tentative d'ajouter un objet null
+	 */
+	public static final int INSERT_OBJECT_NULL=10000;
+
+	/**
+	 * Echec général quand erreur non gérée à l'insertion
+	 */
+	public static final int INSERT_OBJECT_ECHEC=10001;
+
+	/**
+	 * Echec lors de la lecture
+	 */
+	public static final int LECTURE_ECHEC=10002;
+	
+}
